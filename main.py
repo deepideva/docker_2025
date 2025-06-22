d0b1dda812a8070a2c9cb76b5b4c641888fe6093
deepideva <ganesh.learner.2023@gmail.com>
2025-06-22 12:31:22 +0530

    COrrected script

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ def calc():
     a = float(input("First number: "))
     op = input("Operation (+ - * /): ").strip()
     b = float(input("Second number: "))
-    if op == '+':  res = a + b
-    elif op == '-': res = a - b
-    elif op == '*': res = a * b
-    elif op == '/': res = a / b
-    else: raise ValueError("Unknown operator")
+    if op == '+':
+        res = a + b
+    elif op == '-':
+        res = a - b
+    elif op == '*':
+        res = a * b
+    elif op == '/':
+        res = a / b
+    else:
+        raise ValueError("Unknown operator")
     print(f"Result: {res}")
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     calc()
+
